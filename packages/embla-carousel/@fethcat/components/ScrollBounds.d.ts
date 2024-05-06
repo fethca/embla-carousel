63c2eea9c280aa209a820bed1b91d878d83bbb86
fethca <jay.grijol@gmail.com>
2024-05-06 19:56:17 +0200

    fix

---
+++ packages/embla-carousel/@fethcat/components/ScrollBounds.d.ts
@@ -0,0 +1,9 @@
+import { LimitType } from './Limit.js';
+import { PercentOfViewType } from './PercentOfView.js';
+import { ScrollBodyType } from './ScrollBody.js';
+import { Vector1DType } from './Vector1d.js';
+export type ScrollBoundsType = {
+    constrain: (pointerDown: boolean) => void;
+    toggleActive: (active: boolean) => void;
+};
+export declare function ScrollBounds(limit: LimitType, offsetLocation: Vector1DType, target: Vector1DType, scrollBody: ScrollBodyType, percentOfView: PercentOfViewType): ScrollBoundsType;
---
+++ packages/embla-carousel/@fethcat/index.d.ts
@@ -0,0 +1,4 @@
+export { EmblaCarouselType, default } from './components/EmblaCarousel.js';
+export { EmblaEventType } from './components/EventHandler.js';
+export { EmblaOptionsType } from './components/Options.js';
+export { EmblaPluginType } from './components/Plugins.js';
