822dae7a04e92f1164a6536e359a6637d2b7edfd
Fethca <31514619+fethca@users.noreply.github.com>
2024-05-06 19:05:52 +0200

    fix: nodenext compatibility partial test (#1)

--- packages/embla-carousel-react/src/index.ts
+++ packages/embla-carousel-react/src/index.ts
@@ -1,2 +1,1 @@
-export { UseEmblaCarouselType } from './components/useEmblaCarousel'
-export { default } from './components/useEmblaCarousel'
+export { UseEmblaCarouselType, default } from './components/useEmblaCarousel.js'
